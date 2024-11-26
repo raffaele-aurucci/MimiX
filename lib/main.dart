c847903e66dbce0d0384e945c487e273fea4d336
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2024-11-26 08:19:41 +0100

    adding form fields

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@
 import 'package:mimix_app/utils/storage/database_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:mimix_app/utils/view/app_theme.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:mimix_app/user_management/view/registration_page.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -34,20 +36,8 @@ class MimixApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mimix App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-      ),
-      home: isLoggedIn ? const HomePage() : const RegisterPage(),
+      theme: AppTheme.lightTheme,
+      home: isLoggedIn ? const HomePage() : const RegistrationPage(),
     );
   }
 }
