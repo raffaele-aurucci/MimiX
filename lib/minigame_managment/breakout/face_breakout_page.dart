4b3a649ba8e8d77eb1dbef1a5560003c35ecfeb9
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2024-12-07 20:40:08 +0100

    fix

--- lib/minigame_managment/breakout/face_breakout_page.dart
+++ lib/minigame_managment/breakout/face_breakout_page.dart
@@ -5,36 +5,12 @@ import 'package:mimix_app/minigame_managment/breakout/src/breakout.dart';
 import 'package:mimix_app/utils/view/widgets/buttons/primary_button.dart';
 
 import 'package:mimix_app/utils/view/app_palette.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import '../../expression_management/beans/expression_scores.dart';
 import '../../utils/view/widgets/buttons/icon_button.dart';
 import '../../utils/view/widgets/texts/description_text.dart';
 import '../../utils/view/widgets/texts/header_text.dart';
 import '../../utils/view/widgets/alert_dialog.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Permission.camera.request();
-  await Permission.microphone.request();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-          useMaterial3: true,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: const FaceBreakoutPage());
-  }
-}
 
 class FaceBreakoutPage extends StatefulWidget {
   const FaceBreakoutPage({super.key});
