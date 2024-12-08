b7f57ba212e2cb15c6722c033c6d8e3b65680622
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2024-12-08 19:26:51 +0100

    add user icon

--- lib/minigame_managment/breakout/face_breakout_game_page.dart
+++ lib/minigame_managment/breakout/face_breakout_game_page.dart
@@ -163,7 +163,7 @@ class _FaceBreakoutGamePageState extends State<FaceBreakoutGamePage> {
                           ProfileImageWithLevel(
                               experienceLevel: context.watch<UserProvider>().user!.level,
                               experienceProgress: context.watch<UserProvider>().user!.levelProgress + 0.2,
-                              profileImage: const AssetImage('assets/images/welcome.png'),
+                              profileImage: const AssetImage('assets/images/user.png'),
                           )
                         ],
                       )
--- lib/user_management/view/home_page.dart
+++ lib/user_management/view/home_page.dart
@@ -30,7 +30,7 @@ class HomePage extends StatelessWidget {
                     ProfileImageWithLevel(
                       experienceLevel: context.watch<UserProvider>().user!.level,
                       experienceProgress: context.watch<UserProvider>().user!.levelProgress + 0.2,
-                      profileImage: const AssetImage('assets/images/welcome.png'),
+                      profileImage: const AssetImage('assets/images/user.png'),
                     )
                   ],),
                   const SizedBox(height: 20),
