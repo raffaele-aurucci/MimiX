9a7c8f4ad36b4d8dd4ab75c8aa1a05330043ae12
CSSabino <sabinomalafronte27@gmail.com>
2025-01-11 17:01:35 +0100

    Updating progress bar: added orientation

--- lib/utils/view/widgets/progress_bar.dart
+++ lib/utils/view/widgets/progress_bar.dart
@@ -3,38 +3,70 @@ import 'package:mimix_app/utils/view/app_palette.dart';
 
 class ProgressBar extends StatelessWidget {
 
-  static const double smallCard = 7; // Width of progress bar in small cards
-  static const double bigCard = 12; // Width of progress bar in big cards
+  static const double heightSmallCard = 7; // Height of progress bar in small cards
+  static const double heightBigCard = 12; // Height of progress bar in big cards
+  static const double statsProgressBarWidth = 150; // Width of progress bar for stats page
+  static const String vertical = "vertical"; // Vertical orientation of progress bar
+  static const String horizontal = "horizontal"; // Horizontal orientation of progress bar
 
   final double progress; // % progress of bar (double type)
   final double height;
+  final double? width;
+  final String orientation;
 
   const ProgressBar({
     Key? key,
     required this.progress,
-    required this.height
+    required this.height,
+    this.width,
+    required this.orientation,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height, // To edit
-      decoration: BoxDecoration(
-        color: PaletteColor.progressBarBackground,
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16.0),
-        child: LinearProgressIndicator(
+    if(orientation == horizontal) {
+      return Container(
+        height: height, // To edit
+        decoration: BoxDecoration(
+          color: PaletteColor.progressBarBackground,
           borderRadius: BorderRadius.circular(16.0),
-          value: progress,
-          backgroundColor: PaletteColor.progressBarBackground,
-          valueColor: AlwaysStoppedAnimation<Color>(
-            PaletteColor.lightSkyBlue, // Colore di default della barra
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+          child: LinearProgressIndicator(
+            borderRadius: BorderRadius.circular(16.0),
+            value: progress,
+            backgroundColor: PaletteColor.progressBarBackground,
+            valueColor: AlwaysStoppedAnimation<Color>(
+              PaletteColor.lightSkyBlue, // Colore di default della barra
+            ),
           ),
         ),
-      ),
-    );
+      );
+    } else {
+      return RotatedBox(
+        quarterTurns: -1,
+        child: Container(
+          height: height, // To edit
+          width: width,
+          decoration: BoxDecoration(
+            color: PaletteColor.progressBarBackground,
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16.0),
+            child: LinearProgressIndicator(
+              borderRadius: BorderRadius.circular(16.0),
+              value: progress,
+              backgroundColor: PaletteColor.progressBarBackground,
+              valueColor: AlwaysStoppedAnimation<Color>(
+                PaletteColor.lightSkyBlue, // Colore di default della barra
+              ),
+            ),
+          ),
+        ),
+      );
+    }
   }
 }
 
