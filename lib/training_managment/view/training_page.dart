674a266b273aa4d53091cfdbabbe74487230bd4f
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2025-01-31 00:17:33 +0100

    Changes by utente

--- lib/training_managment/view/training_page.dart
+++ lib/training_managment/view/training_page.dart
@@ -41,8 +41,10 @@ class _TrainingPage extends State<TrainingPage> {
     "Learn to form a natural smile",
     "Master puckering your lips for focused gestures",
     "Practice lowering your bottom lip",
-    "Lorem ipsum dolor sit amen, consec...",
-    "Lorem ipsum dolor sit amen, consec...",
+    "Upgrade now to access this session!",
+    "Upgrade now to access this session!",
+    "Upgrade now to access this session!",
+    "Upgrade now to access this session!",
   ];
 
   List<String> nameTrainingOverviewDescriptionList = [
@@ -52,6 +54,8 @@ class _TrainingPage extends State<TrainingPage> {
     "Smile naturally to engage and strengthen the zygomatic muscles for better expressions.",
     "Pucker your lips tightly to improve the strength and focus of the orbicularis oris.",
     "Lower your bottom lip to refine control of the depressor labii and related muscles.",
+    "",
+    "",
     "",
     ""
   ];
